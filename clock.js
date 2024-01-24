0a12af0864d5ff2ec34bfe6c456eaf010dbb31fb
wonusuh <wonwooseo23@gmail.com>
2024-01-24 13:56:43 +0900

    updated : my game piece now can move diagonally.

--- clock.js
+++ clock.js
@@ -1,50 +1,50 @@
 const clock = document.querySelector(`#clock`);
-const ctx = clock.getContext(`2d`);
+const clockCtx = clock.getContext(`2d`);
 let radius = clock.height / 2;
-ctx.translate(radius, radius);
+clockCtx.translate(radius, radius);
 radius *= 0.90;
 setInterval(drawClock, 1000);
 
 function drawClock() {
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
-  drawTime(ctx, radius);
+  drawFace(clockCtx, radius);
+  drawNumbers(clockCtx, radius);
+  drawTime(clockCtx, radius);
 }
 
-function drawFace(ctx, radius) {
-  const grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+function drawFace(clockCtx, radius) {
+  const grad = clockCtx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
   grad.addColorStop(0, `grey`);
   grad.addColorStop(0.5, `white`);
   grad.addColorStop(1, `gray`);
 
-  ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = `white`;
-  ctx.fill();
+  clockCtx.beginPath();
+  clockCtx.arc(0, 0, radius, 0, 2 * Math.PI);
+  clockCtx.fillStyle = `white`;
+  clockCtx.fill();
 
-  ctx.strokeStyle = grad;
-  ctx.lineWidth = radius * 0.22;
-  ctx.stroke();
+  clockCtx.strokeStyle = grad;
+  clockCtx.lineWidth = radius * 0.22;
+  clockCtx.stroke();
 
-  ctx.beginPath();
-  ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
-  ctx.fillStyle = `black`;
-  ctx.fill();
+  clockCtx.beginPath();
+  clockCtx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
+  clockCtx.fillStyle = `black`;
+  clockCtx.fill();
 }
 
-function drawNumbers(ctx, radius) {
-  ctx.font = radius * 0.30 + "px arial";
-  ctx.textBaseline = "middle";
-  ctx.textAlign = "center";
+function drawNumbers(clockCtx, radius) {
+  clockCtx.font = radius * 0.30 + "px arial";
+  clockCtx.textBaseline = "middle";
+  clockCtx.textAlign = "center";
   for (let num = 1; num < 13; num++) {
     let ang = num * Math.PI / 6;
-    ctx.rotate(ang);
-    ctx.translate(0, -radius * 0.70);
-    ctx.rotate(-ang);
-    ctx.fillText(num.toString(), 0, 0);
-    ctx.rotate(ang);
-    ctx.translate(0, radius * 0.70);
-    ctx.rotate(-ang);
+    clockCtx.rotate(ang);
+    clockCtx.translate(0, -radius * 0.70);
+    clockCtx.rotate(-ang);
+    clockCtx.fillText(num.toString(), 0, 0);
+    clockCtx.rotate(ang);
+    clockCtx.translate(0, radius * 0.70);
+    clockCtx.rotate(-ang);
   }
 }
 
@@ -56,22 +56,22 @@ function drawTime() {
   // hour
   hour = hour % 12;
   hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
-  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+  drawHand(clockCtx, hour, radius * 0.5, radius * 0.07);
   // minute
   minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
-  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+  drawHand(clockCtx, minute, radius * 0.8, radius * 0.07);
   // second
   second = (second * Math.PI / 30);
-  drawHand(ctx, second, radius * 0.8, radius * 0.02);
+  drawHand(clockCtx, second, radius * 0.8, radius * 0.02);
 }
 
-function drawHand(ctx, pos, length, width) {
-  ctx.beginPath();
-  ctx.lineWidth = width;
-  ctx.lineCap = `round`;
-  ctx.moveTo(0, 0);
-  ctx.rotate(pos);
-  ctx.lineTo(0, -length);
-  ctx.stroke();
-  ctx.rotate(-pos);
+function drawHand(clockCtx, pos, length, width) {
+  clockCtx.beginPath();
+  clockCtx.lineWidth = width;
+  clockCtx.lineCap = `round`;
+  clockCtx.moveTo(0, 0);
+  clockCtx.rotate(pos);
+  clockCtx.lineTo(0, -length);
+  clockCtx.stroke();
+  clockCtx.rotate(-pos);
 }
